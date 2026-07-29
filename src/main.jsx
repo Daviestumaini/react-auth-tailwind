b5fb39ddcb8d035830518b6ccdb4c56cf377b0d9
Davies Tumaini <daviestumaini@gmail.com>
2026-07-29 09:50:51 +0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+import { LoginPage } from "./pages/Login.jsx";
+import { RegisterPage } from "./pages/Register.jsx";
+import { DashboardPage } from "./pages/Dashboard.jsx";
+import { LandingPage } from "./pages/Landing.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+        </Route>
+        <Route path="/dashboard" element={<DashboardPage />} />
+      
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
-)
+);
